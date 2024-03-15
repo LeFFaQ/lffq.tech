e06fb82aa423701fad6ba382a7f4bf4a793ac6cd
Danil Romanov <lffqyx@yandex.ru>
2024-03-15 23:41:30 +0700

    Add parsing files, saving to cache, changes in css

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module lffq.tech
 
 go 1.22
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-github/v60 v60.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/yuin/goldmark v1.7.0 // indirect
+	go.abhg.dev/goldmark/frontmatter v0.2.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/oauth2 v0.18.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,90 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"fmt"
+	"github.com/google/go-github/v60/github"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/parser"
+	"go.abhg.dev/goldmark/frontmatter"
+	"golang.org/x/oauth2"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
+var GH_TOKEN = os.Getenv("TOKEN")
+var md = goldmark.New(
+	goldmark.WithExtensions(
+		&frontmatter.Extender{},
+	),
+)
+
+var CACHED_POSTS []BlogContent
+
+func init() {
+	pctx := parser.NewContext()
+
+	ctx := context.Background()
+
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: GH_TOKEN})
+	tc := oauth2.NewClient(ctx, ts)
+
+	gh := github.NewClient(tc)
+
+	_, directory, _, err := gh.Repositories.GetContents(ctx, "LeFFaQ", "blog-content", "", nil)
+
+	if err != nil {
+		log.Print("Something wrong with initial request to repository")
+		fmt.Println(err)
+	}
+
+	bc := BlogContent{}
+
+	for _, content := range directory {
+		if content.GetType() == "dir" {
+			slug := content.GetPath()
+
+			file, _, _, _ := gh.Repositories.GetContents(ctx, "LeFFaQ", "blog-content", slug+"/readme.md", nil)
+			fileContent, _ := file.GetContent()
+
+			meta := Meta{}
+
+			buf := new(bytes.Buffer)
+			if err := md.Convert([]byte(fileContent), buf, parser.WithContext(pctx)); err != nil {
+				log.Print("Something wrong with parsing md to html")
+				log.Fatal(err)
+			}
+
+			fm := frontmatter.Get(pctx)
+			if fm == nil {
+				println("no frontmatter found")
+			}
+			if err := fm.Decode(&meta); err != nil {
+				log.Print("Something wrong with parsing frontmatter")
+				log.Fatal(err)
+			}
+			bc.Slug = slug
+			bc.Meta = meta
+			bc.Content = buf.String()
+
+			CACHED_POSTS = append(CACHED_POSTS, bc)
+		}
+	}
+
+	// Test purpose
+	for _, post := range CACHED_POSTS {
+		fmt.Printf("Post %s \n", post.Slug)
+		fmt.Printf("Title: %s \n", post.Meta.Title)
+		fmt.Printf("Content: %s \n", post.Content)
+	}
+}
+
 func main() {
+	fmt.Println("App is running!")
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", AboutHandler)
@@ -23,7 +100,10 @@ func main() {
 
 func AboutHandler(w http.ResponseWriter, r *http.Request) {
 
-	t, _ := template.ParseFiles("./template/index.gohtml", "./template/about.gohtml")
+	t, _ := template.ParseFiles(
+		"./template/index.gohtml",
+		"./template/about.gohtml",
+		"./template/header.gohtml") // I don't know how to DRY it
 
 	err := t.ExecuteTemplate(w, "index", nil)
 
@@ -33,11 +113,38 @@ func AboutHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func BlogHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("./template/blog.gohtml", "./template/index.html")
+	//data := BlogData{}
 
-	log.Fatal(t.Execute(w, nil))
+	mapped := map[string]Meta{}
+	for _, post := range CACHED_POSTS {
+		mapped[post.Slug] = post.Meta
+		fmt.Printf("%s, %s \n", post.Slug, post.Meta.Title)
+	}
+
+	t, _ := template.ParseFiles(
+		"./template/index.gohtml",
+		"./template/blog.gohtml",
+		"./template/header.gohtml") // I don't know how to DRY it
+
+	_ = t.ExecuteTemplate(w, "index", mapped)
+
 }
 
 func BlogByIdHandler(w http.ResponseWriter, r *http.Request) {
 
+}
+
+type Meta struct {
+	Title string    `yaml:"title"`
+	Desc  string    `yaml:"description"`
+	Cover string    `yaml:"cover"`
+	Date  time.Time `yaml:"date"`
+	Tags  []string  `yaml:"tags"`
+}
+
+// BlogContent Describes parsed content of blog, it's metadata and slug. Used for "cache"
+type BlogContent struct {
+	Slug    string
+	Meta    Meta
+	Content string
 }
